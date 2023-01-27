e015dfb714f43995353b7f2868539880ce1237cb
Isaac Klausmeyer <118828742+iklau2023@users.noreply.github.com>
2023-01-26 20:26:51 -0600

    Add files via upload

---
+++ idle.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+window= tk.Tk()
+
+press=1
+
+score= 0
+ 
+def click(event):
+    label= tk.Label(text=f"Score:{score}")
+    label.pack()
+
+button = tk.Button(
+    text=f"+{press}",
+    width=3,
+    height=2,
+    bg="blue",
+    fg="yellow")
+
+button.bind("<Button-2>", click)
+
+label= tk.Label(text=f"Score:{score}")
+
+button.pack()
+label.pack()
+window.mainloop()
